0f9ae3aa00fdd2e53d04d57a75cdf95a5c392cfe
Nate Rosenblum <nater@maginatics.com>
2013-10-12 14:49:53 -0700

    Insert into memcache when pasting
    Works around eventual consistency of the data store, which frequently
    leads to 404 errors after a paste.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Copyright 2012 Diwaker Gupta
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -44,6 +44,7 @@ class SavePaste(webapp2.RequestHandler):
         paste.id = gen_random_string(8)
         paste.content = self.request.get('content')
         paste.put()
+        memcache.add(paste.id, paste.content)
         self.response.set_cookie('delid', str(paste.key()))
         self.redirect('/' + paste.id)
 
